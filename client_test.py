9dcbbfc33510ceb4442b8a203865ce63f6166f17
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-06-22 23:55:48 -0400

    Refactor client_test.py to fix syntax error and improve readability

--- client_test.py
+++ client_test.py
@@ -8,8 +8,8 @@ class ClientTest(unittest.TestCase):
             'top_ask':{'price':121.2, 'size':36},'timestamp':'2019-02-11 22:06:30.572453','top_bid':{'price':120.48, 'size':109},
             'id':'0.109974697771','stock':'ABC'
           }, 
-            'top_ask':{'price':121.68, 'size':4},'timestamp':'2019-02-11 22:06:30.572453','top_bid':{'price':117.87, 'size':81},
-            'id':'0.109974697771','stock':'DEF'
+            {'top_ask':{'price':121.68, 'size':4},'timestamp':'2019-02-11 22:06:30.572453','top_bid':{'price':117.87, 'size':81},
+            'id':'0.109974697771','stock':'DEF'}
         ]
         # add the assertion for the test case
         for quate in quotes:
@@ -21,9 +21,9 @@ def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
           {
             'top_ask':{'price':119.2, 'size':36},'timestamp':'2019-02-11 22:06:30.572453','top_bid':{'price':120.48, 'size':109},
             'id':'0.109974697771','stock':'ABC'
-          }, 
+          }, {
             'top_ask':{'price':121.68, 'size':4},'timestamp':'2019-02-11 22:06:30.572453','top_bid':{'price':117.87, 'size':81},
-            'id':'0.109974697771','stock':'DEF'
+            'id':'0.109974697771','stock':'DEF'}
         ]
 # add the assertion for the test case
   for quate in quotes:
@@ -35,9 +35,9 @@ def test_getDataPoint_calculatePriceAskGreaterThanBid(self):
             {
               'top_ask':{'price':121.2, 'size':36},'timestamp':'2019-02-11 22:06:30.572453','top_bid':{'price':120.48, 'size':109},
               'id':'0.109974697771','stock':'ABC'
-            }, 
+            }, {
               'top_ask':{'price':117.68, 'size':4},'timestamp':'2019-02-11 22:06:30.572453','top_bid':{'price':117.87, 'size':81},
-              'id':'0.109974697771','stock':'DEF'
+              'id':'0.109974697771','stock':'DEF'}
           ]
     # add the assertion for the test case
     for quate in quotes:
